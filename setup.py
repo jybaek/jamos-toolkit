00e210e2be87abc7aacd9abc97abd7c880c10f07
jybaek <oops.jybaek@gmail.com>
2019-05-28 17:19:59 +0900

    Update directory

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'jamos-toolkit',
+    version          = '1.3',
+    description      = 'Jamos separator',
+    author           = 'Jaeyeon Baek',
+    author_email     = 'oops.jybaek@gmail.com',
+    url              = 'https://github.com/jybaek/jamos-toolkit',
+    download_url     = '',
+    install_requires = [],
+    packages         = find_packages(include=['jamostoolkit*']),
+    keywords         = ['jamos', 'toolkit', 'separator'],
+    python_requires  = '>=3',
+    package_data     = {},
+    zip_safe         = False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
